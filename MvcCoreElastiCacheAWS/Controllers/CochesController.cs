7318b487bc5d1f711d8b41810bdd92f079ee9b85
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-31 13:16:50 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Controllers/CochesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreElastiCacheAWS.Models;
+using MvcCoreElastiCacheAWS.Repositories;
+using MvcCoreElastiCacheAWS.Services;
+
+namespace MvcCoreElastiCacheAWS.Controllers
+{
+    public class CochesController : Controller
+    {
+        private ServiceAWSCache serviceCache;
+
+        private RepositoryCoches repo;
+
+        public CochesController(ServiceAWSCache serviceCache, RepositoryCoches repo)
+        {
+            this.serviceCache = serviceCache;
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Coche> coches = this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Coche car = this.repo.FindCoche(id);
+            return View(car);
+        }
+
+        public async Task<IActionResult> SeleccionarFavorito
+    (int idcoche)
+        {
+            //DEBEMOS BUSCAR EL COCHE A ALMACENAR DENTRO DEL XML
+            Coche car = this.repo.FindCoche(idcoche);
+            await this.serviceCache.AddCocheAsync(car);
+            return RedirectToAction("Favoritos");
+        }
+
+        public async Task<IActionResult> Favoritos()
+        {
+            List<Coche> coches = await this.serviceCache.GetCochesFavoritosAsync();
+            return View(coches);
+        }
+
+        public async Task<IActionResult> EliminarFavorito(int idcoche)
+        {
+            await this.serviceCache.DeleteCocheFavoritoAsync(idcoche);
+            return RedirectToAction("Favoritos");
+        }
+
+
+
+    }
+}
---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection =
+       new Lazy<ConnectionMultiplexer>(() =>
+       {
+            //AQUI ES DONDE IRA LA CADENA DE CONEXION
+           return ConnectionMultiplexer.Connect("cache-coches.9nvvhu.ng.0001.use1.cache.amazonaws.com:6379");
+       });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get { return CreateConnection.Value; }
+        }
+
+
+    }
+}
